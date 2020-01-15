7e67315f38f58c883a7590c33d44623030b9e1df
leonardomesa <55280999+leonardomesa@users.noreply.github.com>
2020-01-15 08:31:13 -0500

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+    var x = 1;
+        for(var loop = 0 ;loop < 256; loop++)
+            x *= base
+            return x;
+        }
+
+    Document.write(x);
+</script>
+
+</body>
+</html>
