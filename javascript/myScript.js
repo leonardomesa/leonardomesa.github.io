ea659e7de32d2a29d8c27d43af9d1dc726e341d0
leonardomesa <55280999+leonardomesa@users.noreply.github.com>
2020-01-15 08:50:40 -0500

    Uu

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<script src = "index.html">
-    var x = 1;
-        for(var loop = 0 ;loop < 256; loop++)
-            x *= base
-            return x;
-        }
-
-    Document.write(x);
-</script>
-
-</body>
-</html>
+var x = 1;
+  for(var loop = 0 ;loop < 256; loop++)
+   {
+       var = x *= base
+       return x;
+    }
+Document.write(x);
